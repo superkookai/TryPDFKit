3220698e12a12fc9a385ef8fe157fa4dddf9ba1e
superkookai <weerawut@live.com>
2025-08-17 18:19:15 +0700

    Initial Commit

---
+++ TryPDFKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TryPDFKit
+//
+//  Created by Weerawut Chaiyasomboon on 17/08/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TryPDFKit/TryPDFKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryPDFKitApp.swift
+//  TryPDFKit
+//
+//  Created by Weerawut Chaiyasomboon on 17/08/2568.
+//
+
+import SwiftUI
+
+@main
+struct TryPDFKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
